98927b2202634b7f438e467c52c84543df75a578
Erick Martinez <erickm124@gmail.com>
2023-07-12 18:09:12 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,116 @@
 import { Component } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ChangeDetectorRef } from '@angular/core';
+
+interface Producto {
+  id: number;
+  name: string;
+  imageUrl: string;
+  unitValue: number;
+}
+
+interface ProductoVenta {
+  id: number;
+  name: string;
+  amount: number;
+  unitValue: number;
+}
+
+interface Venta {
+  cliente: {
+    cedula: string;
+    direccion: string;
+  };
+  productosCliente: ProductoVenta[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService, ConfirmationService]
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'Cosmeticos Belleza';
+  cedula: string = '';
+  direccion: string = '';
+  productos: Producto[] = [
+    { id:1, name: 'Producto 1', imageUrl:'https://ionicframework.com/docs/img/demos/card-media.png', unitValue: 3000},
+    { id:2, name: 'Producto 2', imageUrl:'https://ionicframework.com/docs/img/demos/card-media.png', unitValue: 1000 },
+    { id:3, name: 'Producto 3', imageUrl:'https://ionicframework.com/docs/img/demos/card-media.png', unitValue: 2000},
+  ];
+  ventas: Venta = {
+    cliente:{
+      cedula: '',
+      direccion: '',
+    },
+    productosCliente:[]
+  };
+  total: number = 0;
+
+  constructor(private cdRef:ChangeDetectorRef, private messageService: MessageService, private confirmationService: ConfirmationService) {}
+  ngAfterViewChecked()
+  {
+    this.cdRef.detectChanges();
+  }
+
+  calcularTotal() {
+    let total = 0;
+    for (const producto of this.ventas.productosCliente) {
+      total += producto.amount*producto.unitValue;
+    }
+    this.total = total;
+
+    return this.total;
+  }
+
+  guardarPedido() {
+
+    if (this.ventas.cliente.cedula == null || this.ventas.cliente.direccion == null || this.ventas.cliente.cedula == '' || this.ventas.cliente.direccion == '') {
+      this.messageService.add({ severity: 'error', summary: 'Error!', detail: 'La información del cliente esta incompleta.', life: 3000 });
+    }else{
+      this.messageService.add({ severity: 'success', summary: 'Ok!', detail: 'Venta registrada correctamente.', life: 3000 }); 
+       this.reset();
+    }    
+  }
+
+  reset(){
+    this.ventas = {
+      cliente:{
+        cedula:'',
+        direccion:''
+      },
+      productosCliente:[]
+    }  
+  }
+
+  addProd(producto: any) {
+  
+    var index = this.ventas.productosCliente.findIndex(x => x.id==producto.id); 
+    
+    if (index == -1) {
+      let data = {
+        id: producto.id,
+        name: producto.name,
+        amount: 1,
+        unitValue: producto.unitValue
+      }
+      this.ventas.productosCliente.push(data);
+    }else{
+      this.ventas.productosCliente[index].amount++;
+    }    
+  }
+
+  deleteProduct(producto: Producto) {
+    this.confirmationService.confirm({
+        message: 'Esta seguro de borrar el producto: ' + producto.name + '?',
+        header: 'Confirm',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () => {
+          this.ventas.productosCliente = this.ventas.productosCliente.filter((val) => val.id !== producto.id);
+          this.messageService.add({ severity: 'success', summary: 'Ok!', detail: 'Producto eliminado.', life: 3000 });
+        }
+    });
+}
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { TableModule } from 'primeng/table';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    TableModule,
+    InputNumberModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    BrowserAnimationsModule,
+    ConfirmDialogModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
